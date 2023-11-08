f7da4f0171c54bc4685532537fe1bff0f3dddb91
aravindhfinix <aravindh@sparkouttech.com>
2023-11-08 09:59:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { useState, useCallback } from 'react';
+import './App.css';
+import MapWithGeohashes from './Geohash/MapWithGeohashes';
+
+function App() {
+  const [geohashInput, setGeohashInput] = useState('t9ywx');
+  const [staticCoordinatesInput, setStaticCoordinatesInput] = useState(''); // New input for static coordinates
+  const [geohashList, setGeohashList] = useState([]);
+  const [staticCoordinates, setStaticCoordinates] = useState([
+    [11.072102914349513, 76.97078501124118],
+    [11.07024978115282, 76.95636545557711],
+    [11.060983939304466, 76.96220194239352],
+    [11.052054760004486, 76.95756708521579],
+    [11.04144047633279, 76.95945536036227],
+    [11.032510702396053, 76.96323191065524],
+    [11.027624484802901, 76.9637468947861],
+    [11.022401196885015, 76.96572100062106],
+    [11.022569691491652, 76.97121416468356],
+    [11.03613319058459, 76.99533258814547],
+    [11.042872584625423, 76.99254309076998],
+    [11.048432468252146, 76.99679170984957],
+    [11.051517737016189, 77.00178061861727],
+    [11.053086683771422, 76.99372326273654],
+    [11.052128468965414, 76.98497926134799],
+    [11.055045222977125, 76.97486196894381],
+    [11.063574192784497, 76.97179352183078],
+    [11.072102914349513, 76.97078501124118],
+  ]);
+
+  const handleInputChange = useCallback((event) => {
+    setGeohashInput(event.target.value);
+  }, []);
+
+  const handleStaticCoordinatesInputChange = useCallback((event) => {
+    setStaticCoordinatesInput(event.target.value);
+  }, []);
+
+  const handleAddGeohashes = useCallback(() => {
+    const geohashes = geohashInput.split(',').map((item) => item.trim());
+    setGeohashList(geohashes);
+  }, [geohashInput]);
+
+  const handleAddStaticCoordinates = useCallback(() => {
+    try {
+      // Remove the first three and last three characters (brackets and spaces)
+      const trimmedInput = staticCoordinatesInput.trim().slice(3, -3);
+      // Split the trimmed input into individual coordinate pairs and map them
+      const coordinates = trimmedInput.split(',').map((coordinatePair) => {
+        // Split each coordinate pair into latitude and longitude
+        const [longitude, latitude] = coordinatePair.trim().split(' ');
+        return [parseFloat(latitude), parseFloat(longitude)];
+      });
+      setStaticCoordinates(coordinates);
+    } catch (error) {
+      console.error("Error parsing static coordinates:", error);
+    }
+  }, [staticCoordinatesInput]);
+
+  return (
+    <div className="App">
+      <div>
+        <MapWithGeohashes geohashList={geohashList} staticCoordinates={staticCoordinates} />
+        <br />
+        <br />
+        <input
+          type="text"
+          value={geohashInput}
+          onChange={handleInputChange}
+          placeholder="Enter geohashes, separated by commas"
+          style={{ width: '300px' }}
+        />
+        <br />
+        <button onClick={handleAddGeohashes}>Add Geohashes</button>
+
+        <br />
+        <br />
+        <input
+          type="text"
+          value={staticCoordinatesInput}
+          onChange={handleStaticCoordinatesInputChange}
+          placeholder="Enter static coordinates"
+          style={{ width: '300px' }}
+        />
+        <br />
+        <button onClick={handleAddStaticCoordinates}>Add Static Coordinates</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Geohash/MapWithGeohashes.js
@@ -0,0 +1,56 @@
+import React, { useMemo } from 'react';
+import { MapContainer, TileLayer, Polygon, CircleMarker, Popup } from 'react-leaflet';
+import './MapWithGeohashes.css'; // Import a CSS file for custom styles
+import * as geohash from 'ngeohash';
+
+const MapWithGeohashes = React.memo(({ geohashList, staticCoordinates }) => {
+  const polygons = useMemo(() => {
+    return geohashList.map((geohashCode) => {
+      const bounds = geohash.decode_bbox(geohashCode);
+      const polygon = [
+        [bounds[0], bounds[1]],
+        [bounds[0], bounds[3]],
+        [bounds[2], bounds[3]],
+        [bounds[2], bounds[1]],
+      ];
+      return (
+        <Polygon key={geohashCode} positions={polygon}>
+          <Popup>{geohashCode}</Popup>
+        </Polygon>
+      );
+    });
+  }, [geohashList]);
+
+  return (
+    <div>
+      <MapContainer
+        center={staticCoordinates[0]}
+        zoom={13}
+        style={{ height: '70vh', width: '100%' }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        <Polygon positions={staticCoordinates} color="transparent" fillOpacity={0.2} className="custom-polygon" />
+
+        {staticCoordinates.map((coordinate, index) => (
+          <CircleMarker
+            key={index}
+            center={coordinate}
+            radius={1}
+            color="red"
+          >
+            <Popup>
+              <div>
+                <p>Static Coordinate</p>
+              </div>
+            </Popup>
+          </CircleMarker>
+        ))}
+
+        {polygons}
+      </MapContainer>
+    </div>
+  );
+});
+
+export default MapWithGeohashes;
